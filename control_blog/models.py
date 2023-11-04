5fd490a67391e30d91d80cc6ef649c0344f8ebf4
Pilar Kern <pilikern04@gmail.com>
2023-11-04 13:51:30 -0300

    subo modificaciones

--- control_blog/models.py
+++ control_blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Genre(models.Model):
+    name = models.CharField(max_length=64)
+
+class Movies(models.Model):
+    title = models.CharField(max_length=256)
+    length = models.DurationField(null=True)
+    release_year = models.IntegerField()
+    plot = models.TextField(blank=True, null=True)
+
+class Series(models.Model):
+    title = models.CharField(max_length=256)
+    seasons = models.IntegerField(null=True)
+    release_year = models.IntegerField()
+    plot = models.TextField(blank=True, null=True)
